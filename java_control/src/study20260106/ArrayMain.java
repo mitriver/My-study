e2b60a263c600ad9b8931d9e9f8b1f19391dd3cc
DW-203 <DW-203@DESKTOP-FVGV37T>
2026-01-06 14:46:49 +0900

    Merge branch 'main' of https://github.com/mitriver/My-study.git into main

---
+++ java_control/src/study20260106/ArrayMain.java
@@ -0,0 +1,35 @@
+package study20260106;
+
+public class ArrayMain {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[50];
+		// arr 은 참조변수이다 . 참조 변수는 메모리주소를 저장할 수 있는변수이다.
+		
+		System.out.println(arr);
+		int[] arr2 = {10,20,30};
+		
+		
+		
+		
+	}
+
+}
+
+/*
+   배열 - 동일한 데이터타입의  데이터 저장공간이 연속적으로 나열되어있는 구조 
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
