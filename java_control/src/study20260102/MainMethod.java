217afe961ca837d6e6bca32ce1d5c9af7d53c2a3
DW-203 <DW-203@DESKTOP-FVGV37T>
2026-01-06 12:07:13 +0900

    1월6일

--- java_control/src/study20260102/MainMethod.java
+++ java_control/src/study20260102/MainMethod.java
@@ -37,10 +37,18 @@ public class MainMethod {
 		//  단 4시간 이상 주차시 기본요금은 2500원이다
 		//   예) 3시간 50분 (230분) -> 3000원
 		///      4시간 12분 ( 252분) -> 2600원
+<<<<<<< HEAD
 				
 		int cost = 1000;// 기본요금 (30분)
 		int inc = 100; // 10분당 100원
 		int time = 30; // 기본 시간 30분
+=======
+		fees[0];
+		
+		
+		
+		
+>>>>>>> branch 'main' of https://github.com/mitriver/My-study.git
 		
 		// 주차 시간 총 몇분 입력
 		Scanner scan = new Scanner(System.in); // 시스템(컴퓨터)으로부터 입력된키 값 받기 
