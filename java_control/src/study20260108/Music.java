245f9efb0cbdd29070259eb3b53686b04033e07a
DW-203 <DW-203@DESKTOP-FVGV37T>
2026-01-08 17:29:21 +0900

    0108

--- java_control/src/study20260108/Music.java
+++ java_control/src/study20260108/Music.java
@@ -3,15 +3,14 @@ package study20260108;
 public class Music {
 	String musicName;
 	String artist;
-	String playTime;
-
-    Music( String musicName,String artist, String playTime) {
+	int playTime;
+    Music(){}
+    Music( String musicName,String artist, int playTime) {
+    	
     	this.musicName= musicName;
     	this.artist= artist;
     	this.playTime= playTime;
     	
-    	
-    	
     }
-}  
+}
 
