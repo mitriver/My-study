0e9cad22a5129d795a89115cb563d19010a8f1c9
DW-203 <DW-203@DESKTOP-FVGV37T>
2026-01-20 13:53:48 +0900

    0119

---
+++ javaExample/src/exam04/Student.java
@@ -0,0 +1,16 @@
+package exam04;
+
+public class Student {
+	String name;
+	int score;
+	
+	Student(String name, int score){
+		this.name = name;
+		this.score = score;
+	}
+	
+	@Override
+	public String toString() {
+		return name + "(" + score + ")";
+	}
+}
---
+++ javaExample/src/exam07/Student.java
@@ -0,0 +1,70 @@
+package exam07;
+
+public class Student {
+	private String name;
+	private int kor;
+	private int eng;
+	private int mat;
+	private int tot;
+	private double avg;
+	
+	// contructor
+	public Student(String name, int kor, int eng, int mat) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.mat = mat;
+	}
+	
+	public void total() {
+		tot = kor + eng + mat;
+	}
+	
+	public void aver() {
+		avg = tot / 3.0;
+	}
+	
+	//method
+	public void display() {
+	System.out.printf("%s\t%d\t%d\t%d\t%d\t%.2f\n", name, kor, eng, mat); 	 
+	}
+	
+	//getter & setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMat() {
+		return mat;
+	}
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+	public int getTot() {
+		return tot;
+	}
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+	public double getAvg() {
+		return avg;
+	}
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+	
+}
