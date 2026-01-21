3dc48ece6e8c59609b52bc8e0117459e08498112
DW-203 <DW-203@DESKTOP-FVGV37T>
2026-01-21 16:56:46 +0900

    0121

--- javaExample/src/exam13/Friend.java
+++ javaExample/src/exam13/Friend.java
@@ -1,5 +1,13 @@
 package exam13;
 
-public class Friend {
+import exam13.Friend;
 
+public class Friend {
+	String name; // 내 이름 (Data)
+	Friend next; // 내 뒤에 있는 친구 (Address)
+	
+	Friend(String name){
+		this.name = name;
+		this.next = null; // 처음엔 뒤에 아무도 없음
+	}
 }
