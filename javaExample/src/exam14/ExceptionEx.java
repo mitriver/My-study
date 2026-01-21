ab543ff15da28dd1f6cc2aa5bbf1798a223cbea2
DW-203 <DW-203@DESKTOP-FVGV37T>
2026-01-21 18:09:15 +0900

    0121

---
+++ javaExample/src/exam14/ExceptionEx.java
@@ -0,0 +1,24 @@
+package exam14;
+
+public class ExceptionEx {
+	public static void main(String[] args) {
+		int[] numbers = {10, 20, 30};
+		
+	// null처리, 공백처리, 예외처리	
+	try {
+		System.out.println("--- 계산 시작 ---");
+		int result = numbers[0] / 0;
+		System.out.println("값 출력: " + numbers[2]);
+	} catch (ArithmeticException e) {
+		System.out.println("0으로 나눌 수 없음 (" +e.getMessage() + ")");
+	}catch (ArrayIndexOutOfBoundsException e) {
+		System.out.println("존재하지 않는 인텍스");
+	}catch (Exception e) {
+		// 모든 예ㅚ의 조상인 Exception은 가장 마지막에 작성
+		System.out.println("알 수 없는 오류가 발생하였습니다.");
+	}finally {
+		System.out.println(" --- 무조건 실행됨  --- ");
+		}
+		
+	}
+}
