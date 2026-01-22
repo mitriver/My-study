da92050f20918d3a32f5d80e887b32334dd829a4
DW-203 <DW-203@DESKTOP-FVGV37T>
2026-01-22 12:26:17 +0900

    0122

---
+++ javaExample/src/exam22/_Exac.java
@@ -0,0 +1,16 @@
+package exam22;
+
+public class _Exac {
+	public static void main(String[] args) {
+		Downloader downloader = new Downloader();
+		
+		downloader.startDownload(new Downloader.DownloadListener() {
+			
+			@Override
+			public void onComplete(String result) {
+				System.out.println("결과를 받았습니다: " + result);
+				
+			}
+		});
+	}
+}
